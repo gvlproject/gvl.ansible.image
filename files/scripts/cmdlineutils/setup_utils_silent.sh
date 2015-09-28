b09d8fce95c3820410ae6bdbe7c0e565ae1f23bd
nuwan_ag <nuwan.ag@gmail.com>
2015-09-28 17:44:33 +1000

    Moved GVL commandline utilities into an external script

--- files/scripts/cmdlineutils/setup_utils_silent.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-printf "This will download and run scripts to configure GVL command-line utilities, including environment modules, RStudio, and IPython Notebook. Press any key to continue."
-if [ -d "/opt/gvl/gvl_commandline_utilities" ]; then
-   sudo su - ubuntu -c "cd /opt/gvl/gvl_commandline_utilities; source run_all.sh -s"
-fi
